d71a7c8666c980ded7c9ca0f887cc1f8d9816a9e
faketuna <faketuna.st@gmail.com>
2025-11-09 10:38:44 +0900

    feat: todo settime

---
+++ TnmsAdminUtils/Modules/InGameManagement/Commands/GameRules/SetTime.cs
@@ -0,0 +1,51 @@
+﻿using Sharp.Shared.Objects;
+using Sharp.Shared.Types;
+using TnmsPluginFoundation.Models.Command;
+using TnmsPluginFoundation.Models.Command.Validators;
+using TnmsPluginFoundation.Models.Command.Validators.RangedValidators;
+using TnmsPluginFoundation.Utils.Entity;
+
+namespace TnmsAdminUtils.Modules.InGameManagement.Commands.GameRules;
+
+public class SetTime(IServiceProvider provider) : TnmsAbstractCommandBase(provider)
+{
+    public override string CommandName => "settime";
+    public override string CommandDescription => "Sets time of the current round timer.";
+
+    public override TnmsCommandRegistrationType CommandRegistrationType =>
+        TnmsCommandRegistrationType.Client | TnmsCommandRegistrationType.Server;
+
+    protected override ICommandValidator? GetValidator() => new CompositeValidator()
+        .Add(new PermissionValidator("tnms.adminutil.management.ingame.command.settime", true))
+        .Add(new ArgumentCountValidator(1, true))
+        .Add(new RangedArgumentValidator<int>(int.MinValue, int.MaxValue, 1, true));
+
+    protected override ValidationFailureResult OnValidationFailed(ValidationFailureContext context)
+    {
+        switch (context.Validator)
+        {
+            case ArgumentCountValidator:
+                PrintMessageToServerOrPlayerChat(context.Client, LocalizeWithPluginPrefix(context.Client, "SetTime.Notification.Usage"));
+                break;
+            case PermissionValidator:
+                PrintMessageToServerOrPlayerChat(context.Client, LocalizeWithPluginPrefix(context.Client, "Common.ValidationFailure.NotEnoughPermissions"));
+                break;
+            case IRangedArgumentValidator rangedArgumentValidator:
+                PrintMessageToServerOrPlayerChat(context.Client, LocalizeWithPluginPrefix(context.Client, "Common.ValidationFailure.ArgumentIsMustBeInRange", rangedArgumentValidator.ArgumentIndex, rangedArgumentValidator.GetRangeDescription()));
+                break;
+        }
+        
+        return ValidationFailureResult.SilentAbort();
+    }
+
+    protected override void ExecuteCommand(IGameClient? client, StringCommand commandInfo, ValidatedArguments? validatedArguments)
+    {
+        // TODO() A bit complicated
+        // int setSeconds = validatedArguments!.GetArgument<int>(1);
+        //
+        // Plugin.TnmsLogger.LogAdminActionLocalized(client, "SetTime.Broadcast.TimeSet", setSeconds);
+        // Console.WriteLine($"SetTime.Broadcast.TimeSet: {setSeconds}");
+        //
+        // GameRulesUtil.SetRoundTime(setSeconds);
+    }
+}
